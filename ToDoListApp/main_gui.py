e2074b97867afedc4fc2e232436ba66626998847
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-19 12:40:34 +0200

    ToDoListApp - Add the GUI elements and the implementation for the Edit feature.

--- ToDoListApp/main_gui.py
+++ ToDoListApp/main_gui.py
@@ -5,9 +5,14 @@ label = sg.Text("Type in a ToDo")
 input_box = sg.InputText(tooltip="Enter ToDo",
                          key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=helper_functions.getToDos(),
+                      key="todos_list",
+                      enable_events=True,
+                      size=[45, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My ToDoList App",
-                   layout=[[label, input_box, add_button]],
+                   layout=[[label, input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 12))
 
 while True:
@@ -18,6 +23,19 @@ while True:
             todos = helper_functions.getToDos()
             todos.append(value['todo'] + '\n')
             helper_functions.writeToDos(todos)
+            window['todos_list'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = value["todos_list"][0]
+            new_todo = value["todo"] + '\n'
+            todos = helper_functions.getToDos()
+            todo_to_edit_index = todos.index(todo_to_edit)
+            todos[todo_to_edit_index] = new_todo
+            helper_functions.writeToDos(todos)
+            window['todos_list'].update(values=todos)
+
+        case 'todos_list':
+            window['todo'].update(value=value['todos_list'][0])
 
         case sg.WIN_CLOSED:
             break
