aaa8954e34ed47d89ffa330aabb449114dde1157
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-19 11:28:02 +0200

    ToDoListApp - Add functionality for the Add button in the GUI. Adjust GUI's font. Add functionality for stopping the application without errors.

--- ToDoListApp/main_gui.py
+++ ToDoListApp/main_gui.py
@@ -2,9 +2,24 @@ import helper_functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a ToDo")
-input_box = sg.InputText(tooltip="Enter ToDo")
+input_box = sg.InputText(tooltip="Enter ToDo",
+                         key="todo")
 add_button = sg.Button("Add")
-window = sg.Window("My ToDoList App", layout=[[label, input_box, add_button]])
 
-window.read()
+window = sg.Window("My ToDoList App",
+                   layout=[[label, input_box, add_button]],
+                   font=('Helvetica', 12))
+
+while True:
+    event, value = window.read()
+
+    match event:
+        case "Add":
+            todos = helper_functions.getToDos()
+            todos.append(value['todo'] + '\n')
+            helper_functions.writeToDos(todos)
+
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
