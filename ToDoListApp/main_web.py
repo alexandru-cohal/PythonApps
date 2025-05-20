b266e38b7b2581248c6637528c7b0f3e02b788a1
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-20 09:20:02 +0200

    ToDoListApp - Implement the addition of new ToDos for the Web App.

--- ToDoListApp/main_web.py
+++ ToDoListApp/main_web.py
@@ -3,6 +3,13 @@ import helper_functions
 
 todos = helper_functions.getToDos()
 
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    helper_functions.writeToDos(todos)
+
+
 st.title("My ToDoList App")
 st.subheader("This is my ToDoList App")
 st.write("This App increases your productivity")
@@ -11,4 +18,6 @@ for todo in todos:
     st.checkbox(todo)
 
 st.text_input(label="Enter a ToDo:",
-              placeholder="Add a new ToDo")
+              placeholder="Add a new ToDo",
+              on_change=add_todo,
+              key="new_todo")
