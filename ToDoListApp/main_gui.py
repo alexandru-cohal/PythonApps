6d7b0f5c409c7bd96965595c73dab9f4771bde5d
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-20 14:36:14 +0200

    ToDoListApp - Refactor implementation and improve frontend for the GUI

--- ToDoListApp/main_gui.py
+++ ToDoListApp/main_gui.py
@@ -3,36 +3,52 @@ import FreeSimpleGUI as sg
 import time
 import os
 
-if not os.path.exists("todos.txt"):
-    with open("todos.txt", "w") as file:
+GUI_READ_PERIOD_MS = 500
+
+# Create empty "todos.txt" file if not already existent
+if not os.path.exists(helper_functions.FILEPATH_DEFAULT):
+    with open(helper_functions.FILEPATH_DEFAULT, "w") as file:
         pass
 
-sg.theme("Black")
+# Frontend
+sg.theme("DarkGrey10")
 
-label_time = sg.Text("", key="time")
-
-label = sg.Text("Type in a ToDo")
+label_time = sg.Text("Current Date and Time:",
+                     font=("Helvetica", 12, "bold"))
+label_current_time = sg.Text("", key="time")
+label_new_todo = sg.Text("New ToDo:",
+                         font=("Helvetica", 12, "bold"))
 input_box = sg.InputText(tooltip="Enter ToDo",
-                         key="todo")
-add_button = sg.Button("Add")
+                         key="todo",
+                         size=(47,1))
+add_button = sg.Button("Add",
+                       size=(10,1))
+label_current_todo = sg.Text("Current ToDos:",
+                             font=("Helvetica", 12, "bold"))
 list_box = sg.Listbox(values=helper_functions.getToDos(),
                       key="todos_list",
                       enable_events=True,
-                      size=[45, 10])
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
-
-exit_button = sg.Button("Exit")
-
-window = sg.Window("My ToDoList App",
+                      size=(45,10))
+edit_button = sg.Button("Edit",
+                        size=(10,1))
+complete_button = sg.Button("Complete",
+                            size=(10,1))
+column_button = sg.Column([[edit_button], [complete_button]])
+exit_button = sg.Button("Exit",
+                        size=(10,1))
+window = sg.Window("ToDoList App",
                    layout=[[label_time],
-                           [label, input_box, add_button],
-                           [list_box, edit_button, complete_button],
+                           [label_current_time],
+                           [label_new_todo],
+                           [input_box, add_button],
+                           [label_current_todo],
+                           [list_box, column_button],
                            [exit_button]],
                    font=('Helvetica', 12))
 
+# Backend
 while True:
-    event, value = window.read(timeout=500)
+    event, value = window.read(timeout=GUI_READ_PERIOD_MS)
 
     window["time"].update(value=time.strftime("%d %b %Y, %H:%M:%S"))
 
