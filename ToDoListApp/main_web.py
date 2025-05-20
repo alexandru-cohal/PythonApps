5478fa685967bbb02621cc45a5701657282e81ab
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-20 14:35:38 +0200

    ToDoListApp - Refactor implementation and improve frontend for the Web App

--- ToDoListApp/main_web.py
+++ ToDoListApp/main_web.py
@@ -3,27 +3,26 @@ import helper_functions
 
 todos = helper_functions.getToDos()
 
-
 def add_todo():
+    """ Callback function which adds the written To-Do to the list """
     new_todo = st.session_state["new_todo"] + "\n"
     todos.append(new_todo)
     helper_functions.writeToDos(todos)
 
-
-st.title("My ToDoList App")
-st.subheader("This is my ToDoList App")
-st.write("This App increases your productivity")
-
+st.title("ToDoList App")
+st.write("**Current ToDos:**")
+st.write("")
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo,
-                           key=todo)
+    checkbox = st.checkbox(todo, key=todo)
     if checkbox:
         todos.pop(index)
         helper_functions.writeToDos(todos)
         del st.session_state[todo]
         st.rerun()
-
-st.text_input(label="Enter a ToDo:",
-              placeholder="Add a new ToDo",
+st.write("")
+st.write("**New ToDo:**")
+st.text_input(label="New ToDo",
+              label_visibility="hidden",
+              placeholder="Text of the new ToDo",
               on_change=add_todo,
               key="new_todo")
