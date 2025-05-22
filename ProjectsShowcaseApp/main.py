9381990cde8d8948425dada3adee3889cf3a32ad
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-22 15:21:38 +0200

    ProjectsShowcaseApp - Add the list of projects (title, description, image, example link)

--- ProjectsShowcaseApp/main.py
+++ ProjectsShowcaseApp/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -14,3 +15,21 @@ with column2:
 
 content = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
 st.write(content)
+
+projects_df = pandas.read_csv("projects_data.csv", sep=";")
+
+column3, column_empty, column4 = st.columns([1.5, 0.5, 1.5])
+
+with column3:
+    for index, row in projects_df[:2].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("../screenshots/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with column4:
+    for index, row in projects_df[2:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("../screenshots/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
