044eb8275601fb1299478b5b9a6b0550b09390ba
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-27 15:08:54 +0200

    DailyNewsByEmailApp - Send the list of articles' titles and descriptions by email.

--- DailyNewsByEmailApp/main.py
+++ DailyNewsByEmailApp/main.py
@@ -1,10 +1,17 @@
 import requests
 import os
+from send_email import send_email
 
 url = "https://newsapi.org/v2/everything?q=eurovison&sortBy=publishedAt&apiKey=" + os.getenv("NEWS_API_KEY")
 
 req = requests.get(url)
 content = req.json()
 
-for article in content["articles"]:
-    print(article["title"])
+message = ""
+for index, article in enumerate(content["articles"]):
+    message += (str(index) + " - "
+                + article["title"] + "\n"
+                + article["description"] + 2 * "\n")
+
+message = message.encode("UTF-8")
+send_email(message)
