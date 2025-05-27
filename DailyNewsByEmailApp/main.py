213fde8137d20d101d599218e87ae0305fbe3025
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-27 15:25:06 +0200

    DailyNewsByEmailApp - Add links for each article. Limit the number of articles. Filter the articles to be only in English. Add a separate variable for the topic. Add subject to the email.

--- DailyNewsByEmailApp/main.py
+++ DailyNewsByEmailApp/main.py
@@ -2,16 +2,24 @@ import requests
 import os
 from send_email import send_email
 
-url = "https://newsapi.org/v2/everything?q=eurovison&sortBy=publishedAt&apiKey=" + os.getenv("NEWS_API_KEY")
+topic = "eurovison"
+
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "sortBy=publishedAt&"
+       "language=en&"
+       f"apiKey={os.getenv("NEWS_API_KEY")}")
 
 req = requests.get(url)
 content = req.json()
 
-message = ""
-for index, article in enumerate(content["articles"]):
-    message += (str(index) + " - "
-                + article["title"] + "\n"
-                + article["description"] + 2 * "\n")
+message = "Subject: Today's news\n"
+for index, article in enumerate(content["articles"][:20]):
+    if article["title"] is not None:
+        message += (str(index + 1) + " - "
+                    + article["title"] + "\n"
+                    + article["description"] + "\n"
+                    + article["url"] + 2 * "\n")
 
 message = message.encode("UTF-8")
 send_email(message)
