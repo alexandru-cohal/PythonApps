4aad38bd3b329b7cd2ca0abb933cdf5ea0431a4e
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-05-27 12:06:17 +0200

    ExcelToPDFInvoiceGeneratorApp - Add the total price and the generator's name and logo to the generated PDF invoices.

--- ExcelToPDFInvoiceGeneratorApp/main.py
+++ ExcelToPDFInvoiceGeneratorApp/main.py
@@ -17,6 +17,8 @@ for invoice_path in invoices_paths:
     pdf_document.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
     pdf_document.cell(w=50, h=8, txt=f"Date: {invoice_date}", ln=1)
 
+    pdf_document.ln(20)
+
     invoice_content = pd.read_excel(invoice_path, sheet_name="Sheet 1")
 
     table_header = [column.replace("_", " ").title() for column in invoice_content.columns]
@@ -37,6 +39,26 @@ for invoice_path in invoices_paths:
         pdf_document.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf_document.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf_document.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+
+    total_sum = invoice_content["total_price"].sum()
+    pdf_document.set_font(family="Times", size=10, style="B")
+    pdf_document.set_text_color(80, 80, 80)
+    pdf_document.cell(w=30, h=8, txt="", border=1)
+    pdf_document.cell(w=60, h=8, txt="", border=1)
+    pdf_document.cell(w=40, h=8, txt="", border=1)
+    pdf_document.cell(w=30, h=8, txt="", border=1)
+    pdf_document.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf_document.ln(20)
+
+    pdf_document.set_font(family="Times", size=15, style="B")
+    pdf_document.cell(w=60, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    pdf_document.ln(20)
+    pdf_document.cell(w=45, h=10, txt=f"Alexandru Cohal")
+    pdf_document.image("logo.jpg", w=10)
+
 
     pdf_document.output(f"{PDF_INVOICES_PATH}/{invoice_nr}.pdf")
 
