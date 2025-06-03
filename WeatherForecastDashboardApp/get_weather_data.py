325e66356d700a944e56434ccf097f79426874d2
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-03 13:00:28 +0200

    WeatherForecastDashboardApp - Add images of sky types. Add displaying of sky types.

--- WeatherForecastDashboardApp/get_weather_data.py
+++ WeatherForecastDashboardApp/get_weather_data.py
@@ -6,6 +6,7 @@ import os
 
 NUM_ENTRIES_PER_DAY = 8
 
+
 def get_weather_data(location, days, display_option):
     request_url = (f"http://api.openweathermap.org/data/2.5/forecast?"
            f"q={location}&"
@@ -15,12 +16,14 @@ def get_weather_data(location, days, display_option):
     response_raw = response.json()
 
     response_filtered = response_raw["list"][:NUM_ENTRIES_PER_DAY*days]
+
+    timestamps = [response_entry["dt"] for response_entry in response_filtered]
     if display_option == "Temperature":
         weather_data = [response_entry["main"]["temp"] for response_entry in response_filtered]
     else:
         weather_data = [response_entry["weather"][0]["main"] for response_entry in response_filtered]
 
-    return weather_data
+    return timestamps, weather_data
 
 
 if __name__ == "__main__":
--- WeatherForecastDashboardApp/main.py
+++ WeatherForecastDashboardApp/main.py
@@ -2,13 +2,27 @@ import streamlit as st
 import plotly.express as px
 from get_weather_data import get_weather_data
 
+SKY_IMAGES_PATHS = {"Clear":    "images_sky/clear.png",
+                    "Clouds":   "images_sky/cloud.png",
+                    "Rain":     "images_sky/rain.png",
+                    "Snow":     "images_sky/snow.png"}
+
+# Set the GUI and get the needed information
 st.title("Weather Forecast")
 location = st.text_input("Location: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of days for the forecast")
 display_option = st.selectbox("Data to view", options=("Temperature", "Sky"))
 st.subheader(f"{display_option} for the next {days} day(s) in {location}")
 
-dates, temperatures = get_weather_data(location, days, display_option)
+# Get the desired temperature / sky data
+if location:
+    timestamps, weather_data = get_weather_data(location, days, display_option)
 
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    if display_option == "Temperature":
+        # Add the temperature plot
+        figure = px.line(x=timestamps, y=weather_data, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+    else:
+        # Add the sky images
+        sky_images_paths = [SKY_IMAGES_PATHS[sky_type] for sky_type in weather_data]
+        st.image(sky_images_paths, width=150)
