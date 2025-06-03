36d3bda9f5ffb09c7f29b02b13b1c23020519f0c
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-03 12:40:28 +0200

    WeatherForecastDashboardApp - Filter the response received and extract the temperature data or the sky data.

--- WeatherForecastDashboardApp/get_weather_data.py
+++ WeatherForecastDashboardApp/get_weather_data.py
@@ -4,13 +4,24 @@ load_dotenv("../.env")
 import requests
 import os
 
+NUM_ENTRIES_PER_DAY = 8
 
 def get_weather_data(location, days, display_option):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={os.getenv("OPENWEATHERMAP_API_KEY")}"
-    response = requests.get(url)
-    data = response.json()
-    return data
+    request_url = (f"http://api.openweathermap.org/data/2.5/forecast?"
+           f"q={location}&"
+           f"units=metric&"
+           f"appid={os.getenv("OPENWEATHERMAP_API_KEY")}")
+    response = requests.get(request_url)
+    response_raw = response.json()
+
+    response_filtered = response_raw["list"][:NUM_ENTRIES_PER_DAY*days]
+    if display_option == "Temperature":
+        weather_data = [response_entry["main"]["temp"] for response_entry in response_filtered]
+    else:
+        weather_data = [response_entry["weather"][0]["main"] for response_entry in response_filtered]
+
+    return weather_data
 
 
 if __name__ == "__main__":
-    print(get_weather_data("Barcelona", 2, "Temperature"))
+    print(get_weather_data("Barcelona", 2, "Sky"))
