f5d8377e4464e7312dd42ce97776088154124f2f
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-05 15:31:34 +0200

    WebScrapingApp - Add implementation for sending an email when a new event was found. Add periodic execution.

--- WebScrapingApp/main.py
+++ WebScrapingApp/main.py
@@ -1,10 +1,18 @@
+import time
+
+from dotenv import load_dotenv
+load_dotenv("../.env")
+
 import requests
 import selectorlib
+from send_email import send_email
 
 
 URL_TO_SCRAPE = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS_SCRAPING = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 EVENTS_FILEPATH = "events_data.txt"
+WAITING_PERIOD_SEC = 60
+
 
 def scrape_url(url):
     """ Scrape the page source from the given URL """
@@ -20,11 +28,6 @@ def extract_event_information_from_scraped_data(source):
     return value
 
 
-def send_email_new_event():
-    """ Send an email with a new event update """
-    print("Email was sent")
-
-
 def store_event_in_file(extracted):
     """ Store a new event in the events file """
     with open(EVENTS_FILEPATH, "a") as file:
@@ -38,11 +41,19 @@ def read_events_from_file():
 
 
 if __name__ == "__main__":
-    scraped_data = scrape_url(URL_TO_SCRAPE)
-    extracted_data = extract_event_information_from_scraped_data(scraped_data)
+    while True:
+        scraped_data = scrape_url(URL_TO_SCRAPE)
+        extracted_data = extract_event_information_from_scraped_data(scraped_data)
 
-    stored_events = read_events_from_file()
-    if extracted_data != "No upcoming tours":
-        if extracted_data not in stored_events:
-            store_event_in_file(extracted_data)
-            send_email_new_event()
+        stored_events = read_events_from_file()
+        if extracted_data != "No upcoming tours":
+            if extracted_data not in stored_events:
+                store_event_in_file(extracted_data)
+                message = f"""\
+Subject: A new event was found! 
+
+{extracted_data}
+"""
+                send_email(message)
+
+        time.sleep(WAITING_PERIOD_SEC)
---
+++ WebScrapingApp/send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+import os
+
+HOST = "smtp.gmail.com"
+PORT = 465
+SENDER_AND_RECEIVER_EMAIL = os.getenv("EMAIL_AUTOMATION")
+PASSWORD_SENDER_EMAIL = os.getenv("PASS_EMAIL_AUTOMATION")
+
+def send_email(message):
+    """ Function for sending an email with the given message. """
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(HOST, PORT, context=context) as server:
+        server.login(SENDER_AND_RECEIVER_EMAIL, PASSWORD_SENDER_EMAIL)
+        server.sendmail(SENDER_AND_RECEIVER_EMAIL, SENDER_AND_RECEIVER_EMAIL, message)
