15afb2001313ec0c234e4e63592bce71b78762f7
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-10 14:41:27 +0200

    HotelBookingApp - Add implementation of the main function. Add missing class methods. Add file with list of hotels.

--- HotelBookingApp/main.py
+++ HotelBookingApp/main.py
@@ -1,10 +1,38 @@
+import pandas
+
+HOTELS_LIST_FILEPATH = "hotels.csv"
+
+df_hotels = pandas.read_csv(HOTELS_LIST_FILEPATH)
 
 
 class Hotel:
+    def __init__(self, id_hotel):
+        pass
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket:
+    def __init__(self, customer_name, hotel_object):
+        pass
+
     def generate(self):
         pass
+
+
+if __name__ == "__main__":
+    print(df_hotels)
+    id_hotel = input("Enter the ID of the hotel: ")
+    hotel = Hotel(id_hotel)
+
+    if hotel.available():
+        hotel.book()
+        name = input("Enter your name: ")
+        reservation_ticket = ReservationTicket(name, hotel)
+        reservation_ticket.generate()
+    else:
+        print("The hotel is not free")
