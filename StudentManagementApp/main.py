0dd497422c04153d5ec0520be710df5e32c1ff82
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-12 15:49:17 +0200

    StudentManagementApp - Add implementation for the insertion of a new student to the database.

--- StudentManagementApp/main.py
+++ StudentManagementApp/main.py
@@ -1,11 +1,13 @@
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem
+from PyQt6.QtWidgets import QMainWindow, QApplication, QTableWidget, QTableWidgetItem, QDialog, \
+    QVBoxLayout, QLineEdit, QComboBox, QPushButton
 from PyQt6.QtGui import QAction
 import sys
 import sqlite3
 
-# The MainWindow class inherits QMainWindow and not QWidget in order to create divisions within the main window,
-# add a menu bar, a tool bar, a status bar, etc.
+
+# The MainWindow class inherits QMainWindow and not QWidget in order to create divisions within
+# the main window, add a menu bar, a tool bar, a status bar, etc.
 class MainWindow(QMainWindow):
     def __init__(self):
         # Call the __init__ from the parent class
@@ -16,6 +18,7 @@ class MainWindow(QMainWindow):
         # Set the menu bar
         file_menu_item = self.menuBar().addMenu("&File")
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         help_menu_item = self.menuBar().addMenu("&Help")
@@ -37,6 +40,8 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.table)
 
     def load_data(self):
+        """ Load the data from the database and display it in the table from the main window """
+
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
 
@@ -48,6 +53,61 @@ class MainWindow(QMainWindow):
             self.table.insertRow(row_number)
             for column_number, column_data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(column_data)))
+
+    def insert(self):
+        """ Display the insert window """
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        # Add the LineEdit widget for the student's name
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Add the ComboBox widget for the student's course
+        self.course_name = QComboBox()
+        courses = ["Biology", "Math", "Astronomy", "Physics"]
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+
+        # Add the LineEdit widget for the student's mobile number
+        self.mobile_number = QLineEdit()
+        self.mobile_number.setPlaceholderText("Mobile")
+        layout.addWidget(self.mobile_number)
+
+        # Add the submit button
+        button = QPushButton("Register")
+        button.clicked.connect(self.add_student)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def add_student(self):
+        """ Add a new student to the database """
+        name = self.student_name.text()
+        course = self.course_name.itemText(self.course_name.currentIndex())
+        mobile = self.mobile_number.text()
+
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES (?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        # Refresh the displayed table data in the main window
+        app_main_window.load_data()
 
 
 if __name__ == "__main__":
