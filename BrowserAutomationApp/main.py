177a01d990d0f0e084117c364fe6efa11da3cb85
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-13 15:53:56 +0200

    BrowserAutomationApp - Add implementation for downloading a file in the current directory.

--- BrowserAutomationApp/main.py
+++ BrowserAutomationApp/main.py
@@ -4,10 +4,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import os
 
 # Define driver, options and service
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
+
+downloads_path = os.getcwd()
+prefs = {"download.default_directory": downloads_path}
+chrome_options.add_experimental_option("prefs", prefs)
+
 service = Service("chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
@@ -44,6 +50,12 @@ email_field.send_keys("john@gmail.com")
 current_address_field.send_keys("John Street 100, New York, USA")
 permanent_address_field.send_keys("John Street 100, New York, USA")
 driver.execute_script("arguments[0].click();", submit_button)
+
+# Locate the Upload and Download section and the Download button
+upload_download = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-7')))
+upload_download.click()
+download_button = driver.find_element(By.ID, "downloadButton")
+driver.execute_script("arguments[0].click();", download_button)
 
 input("Press Enter to close the browser")
 driver.quit()
