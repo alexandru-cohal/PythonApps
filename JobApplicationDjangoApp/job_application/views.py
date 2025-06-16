16501be60769b90bbf959dd10695b88b3d538816
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-16 16:52:14 +0200

    JobApplicationDjangoApp - Add a form model (forms.py). Use it in order to get the submitted form data (views.py).

--- JobApplicationDjangoApp/job_application/views.py
+++ JobApplicationDjangoApp/job_application/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import JobApplicationForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = JobApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            available_date = form.cleaned_data["available_date"]
+            occupation = form.cleaned_data["occupation"]
+            
     return render(request, "index.html")
