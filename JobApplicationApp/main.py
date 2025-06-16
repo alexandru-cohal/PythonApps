774fa24e4fc5acb87649a093d35b1efb875dd5ca
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-16 13:19:09 +0200

    JobApplicationApp - Get the submitted values from the form. Fix radio buttons and submit button in the index.html file.

--- JobApplicationApp/main.py
+++ JobApplicationApp/main.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Create a Flask app instance
 app = Flask(__name__)
 
 # Handle HTTP requests
 # Home page
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        available_date = request.form["available_date"]
+        occupation = request.form["occupation"]
+        print(first_name, last_name, email, available_date, occupation)
+
     return render_template("index.html")
 
 # Execute the web app
