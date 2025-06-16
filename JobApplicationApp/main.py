6f43a3eb1b7b9e7e0fb03d349801687ac29aff75
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-16 13:29:43 +0200

    JobApplicationApp - Add the creation of the SQLite database needed to store the data submitted by the users. Add the database file.

--- JobApplicationApp/main.py
+++ JobApplicationApp/main.py
@@ -1,7 +1,23 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 # Create a Flask app instance
 app = Flask(__name__)
+# Create a SQLAlchemy database instance
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+# Create the database model
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    available_date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
 
 # Handle HTTP requests
 # Home page
@@ -17,6 +33,11 @@ def index():
 
     return render_template("index.html")
 
-# Execute the web app
-app.run(debug=True,
-        port=5001)
+
+if __name__ == "__main__":
+    with app.app_context():
+        # Create the database
+        db.create_all()
+        # Execute the web app
+        app.run(debug=True,
+                port=5001)
