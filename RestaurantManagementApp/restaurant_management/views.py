fd1f23aefc075669940794a7bcbcc6db52ca5171
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-17 15:44:10 +0200

    RestaurantManagementApp - Add class based views (views.py). Add HTML templates (index.html, menu_item_details.html). Connect the MenuList class based view to the home page url (restaurant_management/urls.py). Link the created app urls.py file in the project urls.py file (mysite/urls.py).

--- RestaurantManagementApp/restaurant_management/views.py
+++ RestaurantManagementApp/restaurant_management/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import MealItem
 
-# Create your views here.
+
+class MenuList(generic.ListView):
+    queryset = MealItem.objects.order_by("-date_created")
+    template_name = "index.html"
+
+
+class MenuItemDetail(generic.DetailView):
+    model = MealItem
+    template_name = "menu_item_details.html"
