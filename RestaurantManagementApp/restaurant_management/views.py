d7a70e57336d29883ba618e2e240939c336b923b
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-18 14:41:24 +0200

    RestaurantManagementApp - Display all the meal items from the menu on the main page (views.py, index.html). Use strike-through style for those items which are not available.

--- RestaurantManagementApp/restaurant_management/views.py
+++ RestaurantManagementApp/restaurant_management/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import MealItem
+from .models import MealItem, MEAL_TYPE
 
 
 class MenuList(generic.ListView):
     queryset = MealItem.objects.order_by("-date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meals": ["Pizza"],
-                   "ingredients": ["Flour", "Salt"]}
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
         return context
 
 
