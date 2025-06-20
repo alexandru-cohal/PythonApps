770d24902483f3c9ea4188cee97371d48d44731d
Alexandru Cohal <alexandru.cohal@yahoo.com>
2025-06-20 09:35:15 +0200

    ProjectsShowcaseApp - Adapt the paths of the CSV and image files in order to cover also the case when the WebApp is deployed (and the root folder is one level above than in the case when the WebApp runs locally).

--- ProjectsShowcaseApp/main.py
+++ ProjectsShowcaseApp/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas
 from math import ceil
+import os
 
 SELF_DESCRIPTION = """Hi! I am Alex! 
 
@@ -17,6 +18,15 @@ repository where I keep all these treasures :)
 Feel free to contact me if you see something interesting or if you have any improvement ideas!"""
 
 PROJECTS_DESCRIPTION_FILEPATH = "projects_data.csv"
+MYSELF_PHOTO_FILEPATH = "images/myself.JPEG"
+SCREENSHOTS_FILEPATH = "../screenshots/"
+# If the projects description CSV file is in the current directory, it means that the WebApp is running locally.
+# Otherwise, it means that the WebApp is deployed and the root folder is one level above.
+if not os.path.isfile(PROJECTS_DESCRIPTION_FILEPATH):
+    # Deployed WebApp
+    PROJECTS_DESCRIPTION_FILEPATH = "ProjectsShowcaseApp/projects_data.csv"
+    MYSELF_PHOTO_FILEPATH = "ProjectsShowcaseApp/images/myself.JPEG"
+    SCREENSHOTS_FILEPATH = "screenshots/"
 
 # Get the projects' information from the separate CSV file
 projects_df = pandas.read_csv(PROJECTS_DESCRIPTION_FILEPATH, sep=",")
@@ -28,7 +38,7 @@ st.set_page_config(layout="wide")
 column_introd_left, column_introd_right = st.columns(2)
 
 with column_introd_left:
-    st.image("images/myself.jpeg", width=500)
+    st.image(MYSELF_PHOTO_FILEPATH)
 
 with column_introd_right:
     st.title("Alexandru Cohal")
@@ -43,12 +53,12 @@ with column_proj_left:
     for index, row in projects_df[:index_middle_projects_df].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("../screenshots/" + row["image"])
+        st.image(SCREENSHOTS_FILEPATH + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with column_proj_right:
     for index, row in projects_df[index_middle_projects_df:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
-        st.image("../screenshots/" + row["image"])
+        st.image(SCREENSHOTS_FILEPATH + row["image"])
         st.write(f"[Source Code]({row['url']})")
